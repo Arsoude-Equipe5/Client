8a7de11f74079c5448cc9d5c5beb647a3e5da785
Ghiles K <1584887@cegepmontpetit.ca>
2024-01-25 10:00:23 -0500

    FEATURE : Display HelloWorld

--- Arsoude-Client/src/app/app.component.ts
+++ Arsoude-Client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+ 
+  async ngOnInit(): Promise<void> {
+    // throw new Error('Method not implemented.');
+
+    let response = await lastValueFrom(this.http.get<any>('http://localhost:5020/api/HelloWorld/HelloWorld'));
+console.log(response.text);
+this.msgRecu = response.text;
+
+  }
+
+  constructor(public http:HttpClient){}
+
   title = 'Arsoude-Client';
+  msgRecu :string= '';
+
 }
