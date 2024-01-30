e3ea0a552c65205f43686591ae9e18eae5416aef
GhilesKo <1584887@cegepmontpetit.ca>
2024-01-30 00:24:49 -0500

    FCT : Validation et requete login

--- Arsoude-Client/src/app/pages/signin/signin.component.ts
+++ Arsoude-Client/src/app/pages/signin/signin.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+  constructor(private auth: AuthService) {}
 
+  onSubmit() {
+    const { email, password } = this.loginForm.value;
+
+    if (email && password) {
+      this.auth.login(email, password).subscribe({
+        next: (res) => {
+          //handle response accordingly
+          console.log(res);
+        },
+        error: (res) => {
+          console.log(res);
+        },
+      });
+    }
+  }
 }
