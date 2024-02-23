2816599bf4b600c16417d013ac15eea48fc21643
Hugo Otth <1968678@cegepmontpetit.ca>
2024-02-23 14:47:38 -0500

    Test build fix

--- Arsoude-Client/src/environments/environment.development.ts
+++ Arsoude-Client/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   env: 'dev',
-  apiUrl: 'https://arsoude-equipe5.azurewebsites.net',
+  //apiUrl: 'https://arsoude-equipe5.azurewebsites.net',
   
-  //apiUrl: 'http://localhost:5020',
+  apiUrl: 'http://localhost:5020',
 };
