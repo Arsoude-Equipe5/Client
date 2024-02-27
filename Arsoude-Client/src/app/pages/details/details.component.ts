77847855920c638a4e87b1427489783c8a1077bf
Laurence <1954868@cegepmontpetit.ca>
2024-02-27 13:26:34 -0500

    FCT: Affichage tile

--- Arsoude-Client/src/app/pages/details/details.component.ts
+++ Arsoude-Client/src/app/pages/details/details.component.ts
@@ -1,5 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core'; // Import Input decorator
 
 import { Subscription } from 'rxjs';
 import { HikeCoordinatesDTO } from 'src/app/models/HikeCoordinatesDTO';
@@ -14,52 +13,35 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent implements AfterViewInit {
-  center: google.maps.LatLngLiteral = { lat: 42, lng: -4 };
-  zoom = 5;
-  hikesList: HikeDTO[] = [];
-  inputKeyword = new FormControl('');
-  tags: string[] = [];
-  searchKeyword: string = '';
-  type: string | null = null;
-
-  status!: string;
-  subscription = new Subscription();
+  @Input() center: google.maps.LatLngLiteral = { lat: 42, lng: -4 }; // Use @Input decorator
+  @Input() zoom: number = 5; // Use @Input decorator
+  markerPositions: google.maps.LatLngLiteral[];
 
   constructor(
     public hikeService: HikeService,
-    private authService: AuthService,
+    private authService: AuthService
   ) {
-    console.log("init hit");
-    
-    this.hikeService.getHikes();
-    this.hikeService.getFavouriteHikes();
-
-    const startPoint1 = new HikeCoordinatesDTO(37.7749, -122.4194, new Date());
-    const endPoint1 = new HikeCoordinatesDTO(40.7128, -74.006, new Date());
-
-    const startPoint2 = new HikeCoordinatesDTO(34.0522, -118.2437, new Date());
-    const endPoint2 = new HikeCoordinatesDTO(41.8781, -87.6298, new Date());
-
-
+    this.markerPositions = []; // Initialize as an empty array
   }
-  ngAfterViewInit(): void {
- 
-  }
+
+  ngAfterViewInit(): void {}
+
   ngOnInit(): void {
+    // Get the full URL
+    const url: string = window.location.href;
 
- 
-    this.hikeService.getHikes();
+    // Split the URL by '/' to get individual parts
+    const parts: string[] = url.split('/');
+
+    // Assuming the ID is the last part of the URL
+    const id: string = parts[parts.length - 1];
+
+    this.hikeService.getSelectedHike(Number(id));
   }
 
   isLoggedIn(): boolean {
     return this.authService.isLoggedIn();
   }
-
-  resetFilters() {
-    this.searchKeyword = '';
-    this.type = null;
-    // Any other filter variables you may have
-}
 
   refreshPage() {
     window.location.reload();
@@ -70,53 +52,17 @@ export class DetailsComponent implements AfterViewInit {
     await this.toggleFavourite(hike);
   }
 
-  setHikeType(type: string) {
-    this.type = type;
-    console.log(this.type);
-  }
-
-  toggleHikeType(type: string) {
-    if (this.type === type) {
-      this.type = null; // Deselect if already selected
-    } else {
-      this.type = type; // Select if not selected
-    }
-  }
-
-  onSubmit() {
-    const token = localStorage.getItem('token');
-
-    if (!token) {
-      alert('You are not connected. Please log in to search hikes.');
-      return; // Stop further execution
-    }
-
-    this.hikeService.searchHikes(this.inputKeyword.value, this.type);
-  }
-
   async toggleFavourite(hike: HikePathDTO): Promise<void> {
-    //Display icon when the hike is not in favourite (empty star)
+    // Display icon when the hike is not in favourite (empty star)
     const isNotInFavouriteIcon: String = 'far fa-regular fa-star';
 
     if (this.hikeService.isInFavourite(hike) === isNotInFavouriteIcon) {
       await this.hikeService.myFavouriteList.push(hike); // Add to favorites
     } else {
       // Remove from favorites
-      this.hikeService.myFavouriteList =
-        this.hikeService.myFavouriteList.filter(
-          (favorite) => favorite.id !== hike.id
-        );
-    }
-  }
-
-  splitKeywords() {
-    if (this.searchKeyword) {
-      // Split the search keyword phrase into individual words
-      const words = this.searchKeyword.split(' ');
-      // Update the tags array with individual words
-      this.tags = words.filter((word) => word.trim() !== '');
-    } else {
-      this.tags = [];
+      this.hikeService.myFavouriteList = this.hikeService.myFavouriteList.filter(
+        (favorite) => favorite.id !== hike.id
+      );
     }
   }
 }
