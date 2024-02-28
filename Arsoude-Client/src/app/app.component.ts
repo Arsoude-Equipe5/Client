27a24d30e39a59752d08134e058349d44576b8d2
2115470 <2115470@cegepmontpetit.ca>
2024-02-27 23:45:48 -0500

    FCT : Konami Code

--- Arsoude-Client/src/app/app.component.ts
+++ Arsoude-Client/src/app/app.component.ts
@@ -1,25 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from 'src/environments/environment';
+import { KonamiService } from './services/konami.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Arsoude-Client';
-
-
-
   language: string = "fr";
 
-  constructor(public translator: TranslateService) {
+  constructor(
+    public translator: TranslateService,
+    private konamiService: KonamiService
+  ) {
     translator.setDefaultLang(this.language);
     console.log(environment);
   }
 
-  changeLanguage(lang:string):void{
+  ngOnInit(): void {
+    this.konamiService.listenForKeyPresses();
+  }
+
+  changeLanguage(lang: string): void {
     this.language = lang;
     this.translator.use(this.language);
   }
